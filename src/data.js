04e9aa1443be177984e6509b2e110a5ff8db9b1b
Szimeonov Viktória <55317291+SzimeonovViktoria@users.noreply.github.com>
2023-03-07 15:04:52 +0100

    upload_project_files

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://ak1.ostkcdn.com/images/products/14052995/Citizen-Mens-CA0349-51L-Eco-Drive-Titanium-Watch-bb2f25f4-96a3-4296-9e25-ad8765c05c85.jpg",
+      title: "smashing sale",
+      desc: "get $250 off woman's watches using our coupons",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://assets-prd.ignimgs.com/2021/06/09/citizen-watch-sale-1623258451553.png",
+      title: "watch it!",
+      desc: "where time matters more",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.pinimg.com/originals/32/43/dc/3243dc52be2de07001174e9a64add952.jpg",
+      title: "perfect style",
+      desc: "all watches -20% this month only",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://www.ap-watches.com/ap-watches/fotos/audemars-piguet-royal-oak-offshore-cronografo-259400k-42mm-1578660624.jpg",
+      title: "WATCHES!",
+      cat:"watch"
+    },
+    {
+      id: 2,
+      img: "https://www.ap-watches.com/ap-watches/fotos/rolex-gmt-master-ii-126710blnr-batman-nuevo-a-estrenar-id15-1642590479.png",
+      title: "CASES",
+      cat:"case"
+    },
+    {
+      id: 3,
+      img: "https://www.ap-watches.com/ap-watches/fotos/rolex-submariner-date-126618lb-oro-amarillo-con-algunos-plasticos-id15-1644339934.png",
+      title: "CRYSTALS",
+      cat:"crystals"
+    },
+  ];
